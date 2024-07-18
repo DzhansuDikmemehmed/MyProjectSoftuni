2aafa935e6e3b625b1e01c07c503d650912d5af0
DzhansuDikmemehmed <dzansudikmemehmed@gmail.com>
2024-07-18 22:43:41 +0300

    Init some changes in the static directory

---
+++ src/main/java/bg/softuni/myproject/controller/HomeController.java
@@ -0,0 +1,29 @@
+package bg.softuni.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/login")
+    public String viewLogin(){
+        return "login";
+    }
+
+
+    @GetMapping("/register")
+    public String viewRegister(){
+        return "register";
+    }
+
+    @GetMapping("/about")
+    public String viewAbout(){
+        return "about";
+    }
+
+    @GetMapping("/profile")
+    public String viewProfile(){
+        return "profile";
+    }
+}
